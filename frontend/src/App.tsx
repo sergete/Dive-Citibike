c394d588177edd2e7ab9181067d49ca856afbe8c
Sergio Sánchez <sergetelopez@gmail.com>
2025-04-26 01:19:51 +0200

    Frontend add modified componentes to run with new interface

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import {Dashboard} from "@/pages/dashboard/Dashboard.tsx";
-import {Route, Routes} from "react-router-dom";
+import {Route, Routes, Navigate} from "react-router-dom";
 import LinksPage from "@/pages/dashboard/links/LinksPage.tsx";
 import StatsPage from "@/pages/dashboard/stats/StatsPage.tsx";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
@@ -12,6 +12,10 @@ export default function App() {
             <QueryClientProvider client={queryClient}>
                 <Dashboard>
                     <Routes>
+                        <Route
+                            path="/"
+                            element={<Navigate to="/links" replace />}
+                        />
                         <Route path='/links' element={<LinksPage/>}/>
                         <Route path='/stats' element={<StatsPage/>}/>
                     </Routes>
